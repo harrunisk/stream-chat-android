2ba0201245f9fd0804e9cac098e77652a4d28dcc
Leandro Borges Ferreira <lehen01@gmail.com>
2020-12-16 20:43:56 +0100

    [CAS-229] Fixing Messages Header View

--- stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/messages/header/MessagesHeaderView.kt
+++ stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/messages/header/MessagesHeaderView.kt
@@ -151,11 +151,20 @@ public class MessagesHeaderView : ConstraintLayout {
 
     private fun configColors() {
         when (resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) {
-            Configuration.UI_MODE_NIGHT_NO -> R.color.stream_ui_white
-            Configuration.UI_MODE_NIGHT_YES -> R.color.stream_ui_dark_background
-            else -> R.color.stream_ui_white
-        }.let { colorRes ->
-            binding.headerRoot.setBackgroundColor(ContextCompat.getColor(context, colorRes))
+            Configuration.UI_MODE_NIGHT_YES -> {
+                binding.headerRoot.setBackgroundColor(
+                    ContextCompat.getColor(
+                        context,
+                        R.color.stream_ui_dark_background
+                    )
+                )
+
+                binding.backButton.setColorFilter(ContextCompat.getColor(context, R.color.stream_ui_white))
+            }
+            else -> {
+                binding.headerRoot.setBackgroundColor(ContextCompat.getColor(context, R.color.stream_ui_white))
+                binding.backButton.setColorFilter(ContextCompat.getColor(context, R.color.stream_ui_black))
+            }
         }
     }
 
@@ -195,7 +204,7 @@ public class MessagesHeaderView : ConstraintLayout {
         )
             .color(
                 R.styleable.MessagesHeaderView_streamUiMessagesHeaderSearchingForNetworkLabelColor,
-                ContextCompat.getColor(context, R.color.stream_ui_text_color_black_translucent)
+                ContextCompat.getColor(context, getTextColor(R.color.stream_ui_text_color_black_translucent))
             )
             .font(
                 R.styleable.MessagesHeaderView_streamUiMessagesHeaderSearchingForNetworkLabelFontAssets,
@@ -206,6 +215,14 @@ public class MessagesHeaderView : ConstraintLayout {
                 Typeface.BOLD
             )
             .build()
+    }
+
+    private fun getTextColor(lightColour: Int): Int {
+        return when (resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) {
+            Configuration.UI_MODE_NIGHT_NO -> lightColour
+            Configuration.UI_MODE_NIGHT_YES -> R.color.stream_ui_white
+            else -> lightColour
+        }
     }
 
     private fun configOfflineLabel(attrs: TypedArray) {
@@ -230,7 +247,7 @@ public class MessagesHeaderView : ConstraintLayout {
         )
             .color(
                 R.styleable.MessagesHeaderView_streamUiMessagesHeaderOfflineLabelTextColor,
-                ContextCompat.getColor(context, R.color.stream_ui_text_color_black_translucent)
+                ContextCompat.getColor(context, getTextColor(R.color.stream_ui_text_color_black_translucent))
             )
             .font(
                 R.styleable.MessagesHeaderView_streamUiMessagesHeaderOfflineLabelFontAssets,
@@ -260,7 +277,7 @@ public class MessagesHeaderView : ConstraintLayout {
         )
             .color(
                 R.styleable.MessagesHeaderView_streamUiMessagesHeaderDefaultLabelTextColor,
-                ContextCompat.getColor(context, R.color.stream_ui_text_color_black_translucent)
+                ContextCompat.getColor(context, getTextColor(R.color.stream_ui_text_color_black_translucent))
             )
             .font(
                 R.styleable.MessagesHeaderView_streamUiMessagesHeaderDefaultLabelFontAssets,
@@ -314,7 +331,7 @@ public class MessagesHeaderView : ConstraintLayout {
         )
             .color(
                 R.styleable.MessagesHeaderView_streamUiMessagesHeaderTitleTextColor,
-                ContextCompat.getColor(context, R.color.stream_ui_black)
+                ContextCompat.getColor(context, getTextColor(R.color.stream_ui_black))
             )
             .font(
                 R.styleable.MessagesHeaderView_streamUiMessagesHeaderTitleFontAssets,
